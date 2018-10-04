ebe665bbd4fbca39c5747acd3fea5d4c72ecba43
mallidii <thinkingpositivly@gmil.com>
2018-10-04 12:25:16 +0530

    practice completed

---
+++ Practice1.py
@@ -0,0 +1,16 @@
+x = 1
+y = 2
+a = 'madhu'
+b = 'mallidi'
+z = a+" "+b
+print(a+" "+b)
+print(x+y)
+print(type(x))
+print(z.strip())
+
+x = float(y)
+print(x)
+
+y = str(x)
+print(y)
+print(type(y))
